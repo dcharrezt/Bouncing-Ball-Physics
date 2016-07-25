a74f08089cc60372449c695b6f3a0627652aed2d
Diego Charrez <d.d.charrez.t@gmail.com>
2016-07-24 19:16:23 -0500

    fixed free falling calculation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    scene = new QGraphicsScene(this);
     QObject::connect(ui->runButton, SIGNAL(clicked()),this, SLOT(processData()) );
     QObject::connect(ui->resetButton, SIGNAL(clicked()), this, SLOT(resetScene()));
 
@@ -21,21 +22,16 @@ MainWindow::~MainWindow()
 
 void MainWindow::drawScene()
 {
-    scene = new QGraphicsScene(this);
-    scene->setSceneRect(0.0,0.0,325,275);
+    scene->setSceneRect(0.0,0.0,320,270);
     ui->graphicsView->setScene(scene);
-
-    std::cout << "WIDTH S " << scene->width() << "HEIGHT " << scene->height() << std::endl;
-    std::cout << "WIDTH " << ui->graphicsView->width() << "HEIGHT " << ui->graphicsView->height() << std::endl;
 
     QRectF rectBall_1(0.0,0.0,50.0,50.0);
     QRectF rectBaLL_2(0.0,0.0,50.0,50.0);
 
     QBrush greenBrush(Qt::darkGreen);
     QBrush blueBrush(Qt::darkBlue);
-
     QPen outlinePen(Qt::black);
-    outlinePen.setWidth(2);
+    outlinePen.setWidth(0);
 
     floor = scene->addRect(0, 0, 320, 30, outlinePen, greenBrush);
     floor->setFlag(QGraphicsItem::ItemIsMovable);
@@ -47,15 +43,25 @@ void MainWindow::drawScene()
     ball_2 = scene->addEllipse(rectBaLL_2, outlinePen, blueBrush);
     ball_2->setFlag(QGraphicsItem::ItemIsMovable);
 
-    //ui->graphicsView->setFixedSize(scene->width(),scene->height());
-    std::cout << "WIDTH S " << scene->width() << "HEIGHT " << scene->height() << std::endl;
-    std::cout << "WIDTH " << ui->graphicsView->width() << "HEIGHT " << ui->graphicsView->height() << std::endl;
-
     ball_1->setPos(75.0,50.0);
     ball_2->setPos(200.0,50.0);
     floor->setPos(0,240);
-    std::cout << ball_1->x() << std::endl;
-    std::cout << floor->x() << " " << floor->y() << std::endl;
+}
+
+FF MainWindow::findVelocityAtTheMoment()
+{
+    ball_1_velocityY = 0 + ball_1_gravity * ball_1_partialTimeElapsed;
+    return ball_1_velocityY;
+}
+
+FF MainWindow::timeGoingUp(FF vel, FF gravity)
+{
+    return ( 2*vel ) / gravity;
+}
+
+FF MainWindow::timeGoingDown(FF height, FF gravity)
+{
+    return sqrt( (2*height)/gravity );
 }
 
 void MainWindow::doTheFuckingMath()
@@ -69,24 +75,28 @@ void MainWindow::processData()
     ball_1_mass = ui->massBox->text().toFloat();
     ball_1_height = ui->heightBox->text().toFloat();
 
+    //time_to_fall = sqrt( (2*ball_1_height) / ball_1_gravity );
+
+
     {
       timer = new QTimer(this);
-      cout << "aagads" << endl;
-      QObject::connect(timer,SIGNAL(timeout()),this,SLOT(advance()));
-      timer->setInterval(15);
+      QObject::connect(timer,SIGNAL(timeout()),this,SLOT(freeFalling()));
+      timer->setInterval(10);
       timer->start();
     }
 
+    time_to_fall = timeGoingDown(ball_1_height, ball_1_gravity);
+    QTimer::singleShot(time_to_fall*1000,this,SLOT(disconnectFall()));
 }
 
 void MainWindow::advance()
 {
-
+    heightBetweenBallFloor = floor->y() - ball_1->y() - ball_1->boundingRect().height();
     if(ball_1_velocityY)
         //The ball doesn't bounce anymore!
 
     // The ball hit the floor
-    if (ball_1->y() + ball_1_velocityY + (ball_1->boundingRect().height() * 0.5) >= floor->y() - (floor->boundingRect().height() * 0.5) - 5)
+    if (ball_1->y() + ball_1_velocityY + (ball_1->boundingRect().height()) > floor->y())
     {
         ball_1_velocityYBeforeImpact = sqrt( 2*ball_1_gravity * ball_1_height);
         std::cout << ball_1_velocityYBeforeImpact << std::endl;
@@ -97,22 +107,55 @@ void MainWindow::advance()
     }
     ball_1->setPos(ball_1->x() + ball_1_velocityX, ball_1->y() + ball_1_velocityY);
 
+    std::cout << heightBetweenBallFloor << std::endl;
 }
 
 void MainWindow::resetScene()
 {
     scene->clear();
-    QObject::disconnect(ui->runButton, SIGNAL(clicked()),this, SLOT(processData()));
-    QObject::disconnect(timer,SIGNAL(timeout()),this,SLOT(advance()));
+    QObject::disconnect(timer,SIGNAL(timeout()),this,SLOT(freeFalling()));
+    ball_1_velocityX = 0.0;
+    ball_1_velocityY = 0.0;
+    ball_1_partialTimeElapsed = 0.0;
     drawScene();
+    heightBetweenBallFloor = floor->y() - ball_1->y() - ball_1->boundingRect().height();
 }
 
 void MainWindow::freeFalling()
 {
+    heightBetweenBallFloor = floor->y() - ( ball_1->y() + ball_1->boundingRect().height() );
+    cout << "VEL " << ball_1_velocityY << endl;
+    ball_1_partialTimeElapsed += 0.001; // miliseconds
+    cout << "PARTIAL: " << ball_1_partialTimeElapsed << endl;
+    cout << "PX PER MILISECOND " << 0.140/time_to_fall << endl;
 
+    if(ball_1_velocityY >= 0)
+        ball_1_velocityY = 1.40/time_to_fall;
+
+    if (ball_1->y() + ball_1_velocityY + (ball_1->boundingRect().height()) > floor->y())
+    {
+        ball_1_velocityYBeforeImpact = sqrt( 2*ball_1_gravity * ball_1_height);
+        ball_1_COR = 0.85;
+        ball_1_velocityYAfterImpact = ball_1_COR * ball_1_velocityY;
+        ball_1_velocityY = -std::abs(ball_1_velocityYAfterImpact);
+        ball_1_gravity = -abs(ball_1_gravity);
+        ball_1_partialTimeElapsed = 0;
+    }
+
+    if ( ball_1_velocityY < 0 ) {
+        ball_1_velocityY = ball_1_velocityY - ball_1_gravity * ball_1_partialTimeElapsed;
+        ball_1->setPos(ball_1->x() + ball_1_velocityX, ball_1->y() + ball_1_velocityY);
+    } else
+        ball_1->setPos(ball_1->x() + ball_1_velocityX, ball_1->y() + ball_1_velocityY);
 }
 
 void MainWindow::bouncingUp()
 {
+    cout << "TESTING "<< time_to_fall <<" SECONDS AFTER " << endl;
+}
 
+void MainWindow::disconnectFall()
+{
+    cout << "TESTING "<< time_to_fall <<" SECONDS AFTER " << endl;
+    QObject::disconnect(timer,SIGNAL(timeout()),this,SLOT(freeFalling()));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -26,6 +26,9 @@ public:
     ~MainWindow();
 
     void drawScene();
+    FF findVelocityAtTheMoment();
+    FF timeGoingUp(FF vel, FF gravity);
+    FF timeGoingDown(FF height, FF gravity);
 
 private:
     Ui::MainWindow *ui;
@@ -38,23 +41,35 @@ private:
 
     QTimer * timer;
 
+    FF heightBetweenBallFloor;
+
+    FF time_to_fall;
+
     FF ball_1_gravity;
     FF ball_1_mass;
     FF ball_1_height;
     FF ball_1_velocityX = 0.0;
-    FF ball_1_velocityY = 1.0;
+    FF ball_1_velocityY = 0.0;
     FF ball_1_COR;
     FF ball_1_KE;
     FF ball_1_PE;
     FF ball_1_velocityYBeforeImpact;
     FF ball_1_velocityYAfterImpact;
-    FF ball_1_timeElapsed = 0.0; // miliseconds
-
+    FF ball_1_partialTimeElapsed = 0.0; // miliseconds
+    FF ball_1_totalTimeElapsed;
 
     FF ball_2_gravity;
     FF ball_2_mass;
     FF ball_2_height;
     FF ball_2_velocity;
+
+    FF COR_golf_ball = 0.858;
+    FF COR_tennis_ball = 0.712;
+    FF COR_billiard_ball = 0.804;
+    FF COR_wooden_ball = 0.603;
+    FF COR_steel_ball = 0.597;
+    FF COR_rubber_ball = 0.828;
+    FF COR_plastic_ball = 0.688;
 
 public slots:
     void doTheFuckingMath();
@@ -63,6 +78,8 @@ public slots:
     void resetScene();
     void freeFalling(); // accelerating
     void bouncingUp(); // decelerating
+
+    void disconnectFall();
 
 };
 
